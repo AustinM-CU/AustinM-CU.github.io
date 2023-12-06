e3a5f54387085510c481e36a5138193c8001f0cc
Austin McClendon <austin.mcclendon@colorado.edu>
2023-12-06 14:05:04 -0700

    update JS Final

--- js-midterm/js-midterm.js
+++ js-midterm/js-midterm.js
@@ -1,6 +1,9 @@
+// Wait for the DOM to be fully loaded before executing the script
 document.addEventListener('DOMContentLoaded', function () {
+    // Number of rectangular canvases
     const numRectangles = 10;
 
+    // Check and create canvas container if it doesn't exist
     let canvasContainer = document.getElementById('canvasContainer');
     if (!canvasContainer) {
         canvasContainer = document.createElement('div');
@@ -8,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.appendChild(canvasContainer);
     }
 
+    // Check and create percentage list container if it doesn't exist
     let percentageListContainer = document.getElementById('percentageContainer');
     if (!percentageListContainer) {
         percentageListContainer = document.createElement('ul');
@@ -15,6 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.appendChild(percentageListContainer);
     }
 
+    // Check and create phone display container if it doesn't exist
     let phoneDisplay = document.getElementById('phoneDisplay');
     if (!phoneDisplay) {
         phoneDisplay = document.createElement('div');
@@ -22,8 +27,10 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.appendChild(phoneDisplay);
     }
 
+    // Array to store percentage displays
     const percentageList = new Array(numRectangles).fill(0);
 
+    // Create canvases and displays, and populate the percentageList array
     for (let i = 1; i <= numRectangles; i++) {
         const { percentageDisplay } = createCanvasAndDisplays(`drawCanvas${i}`, canvasContainer, i - 1);
         percentageList[i - 1] = percentageDisplay;
@@ -32,6 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Initialize the phone number display
     updatePhoneNumber();
 
+    // Function to create canvases and displays
     function createCanvasAndDisplays(canvasId, container, index) {
         const canvas = document.createElement('canvas');
         canvas.id = canvasId;
@@ -50,6 +58,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const percentageDisplay = document.createElement('div');
         percentageListContainer.appendChild(percentageDisplay);
 
+        // Event listeners for drawing on canvases
         canvas.addEventListener('mousedown', function (event) {
             isDrawing = true;
             draw(canvas, event.clientX, event.clientY, imageData.data, totalPixels, percentageDisplay, index);
@@ -74,15 +83,16 @@ document.addEventListener('DOMContentLoaded', function () {
         return { percentageDisplay };
     }
 
+    // Function to draw on canvases and update percentage displays
     function draw(canvas, x, y, imageData, totalPixels, percentageDisplay, index) {
         const ctx = canvas.getContext('2d');
-        const rect = canvas.getBoundingClientRect(); // Get the bounding rectangle of the canvas
+        const rect = canvas.getBoundingClientRect();
 
         // Canvas coordinates
         const canvasX = (x - rect.left) / rect.width * canvas.width;
         const canvasY = (y - rect.top) / rect.height * canvas.height;
 
-        //Make the circle
+        // Make a circle on the canvas
         ctx.fillStyle = '#000';
         ctx.beginPath();
         ctx.arc(canvasX, canvasY, 10, 0, 2 * Math.PI);
@@ -97,10 +107,12 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
 
+        // Calculate and update percentage drawn
         const percentageDrawn = Math.round((filledPixels / totalPixels) * 100);
         percentageDisplay.textContent = `${percentageDrawn}%`;
     }
 
+    // Function to update the phone number based on canvas percentages
     function updatePhoneNumber() {
         // Calculate the phone number based on the percentages of all canvases
         const phoneNumber = percentageList.map(percentage => {
@@ -108,7 +120,7 @@ document.addEventListener('DOMContentLoaded', function () {
             if (isNaN(percentageValue)) {
                 return 'X';
             } else {
-                // Rounding Logic 
+                // Rounding Logic
                 if (percentageValue === 100) {
                     return '0';
                 } else if (percentageValue >= 1 && percentageValue <= 19) {
@@ -130,17 +142,18 @@ document.addEventListener('DOMContentLoaded', function () {
                 } else if (percentageValue >= 90 && percentageValue <= 99) {
                     return 9;
                 }
-            
+
                 // Normal rounding for other cases
                 return Math.round(percentageValue / 10);
             }
-            
+
         }).join('');
 
         // Display the formatted phone number
         phoneDisplay.textContent = formatPhoneNumber(phoneNumber);
     }
 
+    // Function to format the phone number
     function formatPhoneNumber(number) {
         if (number === 'X') {
             return 'XXX-XXX-XXX';
@@ -149,4 +162,40 @@ document.addEventListener('DOMContentLoaded', function () {
         const str = number.toString().padStart(10, '0');
         return `${str.slice(0, 3)}-${str.slice(3, 6)}-${str.slice(6)}`;
     }
-});
+
+    // Create a reset button
+    const resetButton = document.createElement('button');
+    resetButton.textContent = 'Reset Canvases';
+    resetButton.addEventListener('click', resetCanvases);
+    document.body.appendChild(resetButton);
+
+    // Initialize the percentages to 'X%'
+    percentageList.forEach(percentageDisplay => {
+        percentageDisplay.textContent = 'X%';
+    });
+
+    // New function to reset canvases
+    function resetCanvases() {
+        // Clear each canvas and redraw borders
+        for (let i = 1; i <= numRectangles; i++) {
+            const canvas = document.getElementById(`drawCanvas${i}`);
+            const ctx = canvas.getContext('2d');
+
+            // Clear canvas
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            // Redraw borders
+            ctx.strokeStyle = '#ccc';
+            ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+            // Reset percentage display 
+            percentageList[i - 1].textContent = 'X%'
+
+
+    
+    updatePhoneNumber();
+} 
+
+}});
+
+
